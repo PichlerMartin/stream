c9473f5639b3c0785e6e8b8184a4c3597057210e
PichlerMartin <40146720150314@litec.ac.at>
2019-08-09 22:52:33 +0200

    completed StreamClient in testui

--- src/main/java/testui/StreamClient.java
+++ src/main/java/testui/StreamClient.java
@@ -7,6 +7,8 @@ import bt.cli.Options;
 import bt.cli.SessionStatePrinter;
 import bt.data.Storage;
 import bt.data.file.FileSystemStorage;
+import bt.dht.DHTConfig;
+import bt.dht.DHTModule;
 import bt.module.ProtocolModule;
 import bt.protocol.crypto.EncryptionPolicy;
 import bt.runtime.BtClient;
@@ -25,6 +27,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import joptsimple.OptionException;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -42,21 +45,28 @@ public class StreamClient {
     private final SessionStatePrinter printer;
     private final BtClient client;
 
-
+    /**
+     * Description:
+     * Konstruktor der StreamClient Klasse, erstellt alle wichtigen Metadaten wie die Konfiguration,
+     * den Storage und den ClientBuilder
+     *
+     * @param options ist ein Objekt der Options Klasse welche die Kommandozeilenargumente (argsv)
+     *                enthält, dem Client die nötigen Daten gibt, z.B.: Torrent-File, Download Ort, ...
+     * @throws MalformedURLException wird geworfen wenn z.B.: die URL des Torrent-Files nicht existiert
+     */
     public StreamClient(Options options) throws MalformedURLException {
         this.options = options;
         this.printer = new SessionStatePrinter();
 
-        Config config = new Config();
+        Config config = buildConfig(this.options);
 
-        BtRuntime runtime = BtRuntime.builder(config).module(new ProtocolModule()).autoLoadModules().build();
+        BtRuntime runtime = BtRuntime.builder(config).module(buildDHTModule(options)).autoLoadModules().build();
 
-        Storage storage = new FileSystemStorage(options.getTargetDirectory());
-        //  PieceSelector selector = options.downloadSequentially();
-        SequentialSelector.sequential();
-        RarestFirstSelector.randomizedRarest();
+        Storage storage = new FileSystemStorage(options.getTargetDirectory().toPath());
+        PieceSelector selector = options.downloadSequentially() ?
+        SequentialSelector.sequential() : RarestFirstSelector.randomizedRarest();
 
-        BtClientBuilder clientBuilder = Bt.client(runtime).storage(storage);
+        BtClientBuilder clientBuilder = Bt.client(runtime).storage(storage).selector(selector);
 
         if (! options.shouldDownloadAllFiles ()) {
             CliFileSelector fileSelector = new CliFileSelector();
@@ -77,6 +87,30 @@ public class StreamClient {
         }
 
         this.client = clientBuilder.build ();
+    }
+
+    /**
+     * Description
+     * Erstellt das DHT Modul (distributed hash table) für den Port über den der
+     * Download erfolgt
+     *
+     * @param options das Options Objekt
+     * @return
+     */
+    private static DHTModule buildDHTModule(Options options){
+        Optional<Integer> dhtPortOverride = tryGetPort(options.getDhtPort());
+
+        return new DHTModule(new DHTConfig(){
+            @Override
+            public int getListeningPort(){
+                return dhtPortOverride.orElseGet(super::getListeningPort);
+            }
+
+            @Override
+            public boolean shouldUseRouterBootstrap(){
+                return true;
+            }
+        });
     }
 
     private static Config buildConfig (Options options) {
@@ -125,20 +159,45 @@ public class StreamClient {
         return Optional.of (port);
     }
 
-    public static void main(String[] args) {
+    //  ToDo:   Run this and ask hasp for assistance
+    //  ToDo:   Ask hasp which language should be used
+
+    public static void main(String[] args) throws IOException {
         Options options;
 
-        args = new String[] {"-d", "C:\\"};
+        args = new String[] {"-d", "C:\\", "-m", "magnet:?xt=urn:btih:d1eb2b5cf80e286a7f848ab0c31638856db102d4&dn=Beethoven+-+The+Very+Best+Of+Beethoven+%282005%29+%5BFLAC%5D+dussin"};
         try{
             options = Options.parse (args);
         }catch (OptionException e){
             Options.printHelp(System.out);
+            return;
         }
 
-        configureLogging(1);
+        configureLogging(options.getLogLevel());
         configureSecurity();
         registerLog4jShutdownHook();
+
+        StreamClient client = new StreamClient(options);
+        client.start();
     }
+
+    private void start(){
+        printer.start();
+        client.startAsync (state -> {
+            boolean complete = (state.getPiecesRemaining()==0);
+            if (complete){
+                if(options.shouldSeedAfterDownloaded()){
+                    printer.onDownloadComplete();
+                } else{
+                    printer.stop();
+                    client.stop();
+                }
+            }
+
+            printer.updateState(state);
+        }, 1000).join();
+    }
+
 
     private static void configureSecurity(){
         String key = "crypto.policy";
@@ -166,20 +225,20 @@ public class StreamClient {
         });
     }
 
-    private static void configureLogging(int logLevel) {
+    private static void configureLogging(Options.LogLevel logLevel) {
         Level log4jLogLevel;
         switch (logLevel) {
-            case 1: {
+            case NORMAL: {
                 //  NORMAL
                 log4jLogLevel = Level.INFO;
                 break;
             }
-            case 2: {
+            case VERBOSE: {
                 //  VERBOSE
                 log4jLogLevel = Level.DEBUG;
                 break;
             }
-            case 3: {
+            case TRACE: {
                 //  TRACE
                 log4jLogLevel = TRACE;
                 break;
