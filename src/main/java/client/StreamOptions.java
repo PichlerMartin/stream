d7a2751b12d8f3183ae4230fe3c6a259b6f558b8
PichlerMartin <40146720150314@litec.ac.at>
2019-08-13 21:28:37 +0200

    worked on StreamOptions

--- src/main/java/client/StreamOptions.java
+++ src/main/java/client/StreamOptions.java
@@ -1,4 +1,88 @@
 package client;
 
+import java.io.File;
+
 public class StreamOptions {
+    private File metainfoFile;
+    private String magnetUri;
+    private File targetDirectory;
+    private boolean seedAfterDownloaded;
+    private boolean sequential;
+    private boolean enforceEncryption;
+    private boolean verboseLogging;
+    private boolean traceLogging;
+    private String inetAddress;
+    private Integer port;
+    private Integer dhtPort;
+    private boolean downloadAllFiles;
+
+    public StreamOptions(File metainfoFile, String magnetUri, File targetDirectory, boolean seedAfterDownloaded, boolean sequential, boolean enforceEncryption, boolean verboseLogging, boolean traceLogging, String inetAddress, Integer port, Integer dhtPort, boolean downloadAllFiles) {
+        this.metainfoFile = metainfoFile;
+        this.magnetUri = magnetUri;
+        this.targetDirectory = targetDirectory;
+        this.seedAfterDownloaded = seedAfterDownloaded;
+        this.sequential = sequential;
+        this.enforceEncryption = enforceEncryption;
+        this.verboseLogging = verboseLogging;
+        this.traceLogging = traceLogging;
+        this.inetAddress = inetAddress;
+        this.port = port;
+        this.dhtPort = dhtPort;
+        this.downloadAllFiles = downloadAllFiles;
+    }
+
+    public File getMetainfoFile() {
+        return this.metainfoFile;
+    }
+
+    public String getMagnetUri() {
+        return this.magnetUri;
+    }
+
+    public File getTargetDirectory() {
+        return this.targetDirectory;
+    }
+
+    public boolean shouldSeedAfterDownloaded() {
+        return this.seedAfterDownloaded;
+    }
+
+    public boolean downloadSequentially() {
+        return this.sequential;
+    }
+
+    public boolean enforceEncryption() {
+        return this.enforceEncryption;
+    }
+
+    public StreamOptions.LogLevel getLogLevel() {
+        return this.traceLogging ? StreamOptions.LogLevel.TRACE :
+                (this.verboseLogging ? StreamOptions.LogLevel.VERBOSE :
+                        StreamOptions.LogLevel.NORMAL);
+    }
+
+    public String getInetAddress() {
+        return this.inetAddress;
+    }
+
+    public Integer getPort() {
+        return this.port;
+    }
+
+    public Integer getDhtPort() {
+        return this.dhtPort;
+    }
+
+    public boolean shouldDownloadAllFiles() {
+        return this.downloadAllFiles;
+    }
+
+    public enum LogLevel {
+        NORMAL,
+        VERBOSE,
+        TRACE;
+
+        LogLevel() {
+        }
+    }
 }
