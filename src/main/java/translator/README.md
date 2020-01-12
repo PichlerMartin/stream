8ed47ad165ecb348df040fa5129212bf4558f3e5
PichlerMartin <40146720150314@litec.ac.at>
2020-01-12 14:23:41 +0100

    worked on StreamTranslator, marked as deprecated

--- src/main/java/translator/README.md
+++ src/main/java/translator/README.md
@@ -3,3 +3,26 @@
 this package contains the static class StreamTranslator which can be run via a main method. the purpose of this class
 is to read a properties file from the resources directory and translate it to any given language. it aims to simplify
 the process of translating every properties file one by one.
+
+## first attempt
+
+implemented restful webservice by "whatsmate", but this service works only on a limited free trial of 10 translations
+professional usage requires a paid subscription, that's why this method is seen as unsuitable for our needs, and thus
+being pursued no more
+
+https://whatsmate.github.io/2016-08-18-translate-text-java/
+
+## second attempt
+
+tried to use proposed solution from stackoverflow, however this does not work anymore, because the requested server
+which provides this webservice is unreachable, was removed in recent commit
+
+https://stackoverflow.com/questions/8147284/how-to-use-google-translate-api-in-my-java-application
+
+## third attempt
+
+used google cloud services to translate text, but failed due to non-existing authentication credentials
+
+## final solution
+
+proceeded to translate the contents of english resource bundle by hand into spanish and french
--- src/main/java/translator/StreamTranslator.java
+++ src/main/java/translator/StreamTranslator.java
@@ -5,18 +5,35 @@ import com.google.cloud.translate.v3beta1.TranslateTextRequest;
 import com.google.cloud.translate.v3beta1.TranslateTextResponse;
 import com.google.cloud.translate.v3beta1.TranslationServiceClient;
 
+/**
+ * Desc:
+ * see README.md for detailed description
+ */
+@Deprecated
 public abstract class StreamTranslator{
     /**
+     * Desc:
      * Translates a given text to a target language.
      * https://cloud.google.com/translate/docs/quickstart-client-libraries-v3
      *
-     * @param projectId - Id of the project.
+     * the given code was copied from the link below, it works with one of
+     * google's cloud services but fails with an IOException because of missing
+     * application credentials. apparently it is required to create an google
+     * developer account in order to authenticate with the google cloud and
+     * use this library. (second and third link below)
+     *
+     * cloud services account was not created, class marked as deprecated and no
+     * longer in use
+     *
+     * https://cloud.google.com/docs/authentication/production?hl=de
+     * https://cloud.google.com/docs/authentication/getting-started?hl=de
+     *  @param projectId - Id of the project.
      * @param location - location name.
      * @param text - Text for translation.
      * @param sourceLanguageCode - Language code of text. e.g. "en"
      * @param targetLanguageCode - Language code for translation. e.g. "sr"
      */
-    static TranslateTextResponse translateText(
+    private static void translateText(
             String projectId,
             String location,
             String text,
@@ -40,10 +57,18 @@ public abstract class StreamTranslator{
             TranslateTextResponse response = translationServiceClient.translateText(translateTextRequest);
             System.out.format(
                     "Translated Text: %s", response.getTranslationsList().get(0).getTranslatedText());
-            return response;
 
         } catch (Exception e) {
             throw new RuntimeException("Couldn't create client.", e);
         }
+    }
+
+    /**
+     * Desc:
+     * test driver for translateText method above
+     */
+    public static void main() {
+        translateText("test1", "home", "Hello world", "en", "sr");
+        translateText("test2", "somewhere", "Hallo Welt", "de", "en");
     }
 }
