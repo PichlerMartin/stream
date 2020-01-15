a9c9b876ff1757d394915533ce70ac9ff15d7a81
PichlerMartin <40146720150314@litec.ac.at>
2020-01-15 20:40:26 +0100

    finished javadoc on interface Client

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,5 +1,14 @@
 package client;
 
+/**
+ * <p>Client interface for possible alternative implementations of {@link StreamClient}, no
+ * longer pursued/maintained.</p>
+ *
+ * @see StreamClient
+ * @since 0.00.3
+ * @author PichlerMartin
+ */
+@Deprecated
 public interface Client {
 
 }
