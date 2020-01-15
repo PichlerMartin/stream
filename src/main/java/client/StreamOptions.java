a01358b3f1d3edc7532395f0c20b14bbad67410a
PichlerMartin <40146720150314@litec.ac.at>
2020-01-15 14:49:30 +0100

    added javadoc enum LogLevel

--- src/main/java/client/StreamOptions.java
+++ src/main/java/client/StreamOptions.java
@@ -3,7 +3,11 @@ package client;
 import java.io.File;
 
 /**
+ * <p>This class contains a sum of private fields which will be used to instantiate an object of
+ * the StreamClient-class in order to create a torrent-client. More accurate descriptions of the
+ * fields can be found in the corresponding constructors. The methods left serve as getter methods</p>
  *
+ * @since 0.00.2
  */
 public class StreamOptions {
     private File metainfoFile;
@@ -25,18 +29,18 @@ public class StreamOptions {
      * use of all possible parameters in the current version of stream yet. If you want to use one of the
      * additional parameters you will need to write a new constructor with the parameters you need.
      *
-     * @param metainfoFile: a .torrent-file which can be used to provide meta data of a torrent
-     * @param magnetUri: magnet uri/link which can be used as an alternative to metainfoFile
-     * @param targetDirectory: the directory in which the torrent is stored
-     * @param seedAfterDownloaded: boolean, whether the client should seed the torrent after downloading has finished
-     * @param sequential: boolean, defines whether torrent-parts should be downloaded one after another or randomly
-     * @param enforceEncryption: boolean, says whether encrypted connections should be used
-     * @param verboseLogging: defines verbose logging level
-     * @param traceLogging: defines trace logging level
-     * @param inetAddress: a specific internet-address to which the client should connect
-     * @param port: specific port for incoming connections, if empty default port is used
-     * @param dhtPort: specific port for the dht connection
-     * @param downloadAllFiles: boolean, specifies whether all or only some files should be downloaded
+     * @param metainfoFile a .torrent-file which can be used to provide meta data of a torrent
+     * @param magnetUri magnet uri/link which can be used as an alternative to metainfoFile
+     * @param targetDirectory the directory in which the torrent is stored
+     * @param seedAfterDownloaded boolean, whether the client should seed the torrent after downloading has finished
+     * @param sequential boolean, defines whether torrent-parts should be downloaded one after another or randomly
+     * @param enforceEncryption boolean, says whether encrypted connections should be used
+     * @param verboseLogging defines verbose logging level
+     * @param traceLogging defines trace logging level
+     * @param inetAddress a specific internet-address to which the client should connect
+     * @param port specific port for incoming connections, if empty default port is used
+     * @param dhtPort specific port for the dht connection
+     * @param downloadAllFiles boolean, specifies whether all or only some files should be downloaded
      */
     @Deprecated
     public StreamOptions(File metainfoFile, String magnetUri, File targetDirectory, boolean seedAfterDownloaded, boolean sequential, boolean enforceEncryption, boolean verboseLogging, boolean traceLogging, String inetAddress, Integer port, Integer dhtPort, boolean downloadAllFiles) {
@@ -61,14 +65,14 @@ public class StreamOptions {
      * and if the meta information is provided via a magnet uri/link or a .torrent-file
      * </p>
      *
-     * {@link StreamClient}
+     * {@link client.StreamClient#main(String[]) usage of this constructor}
      *
-     * @param metainfoFile: a .torrent-file which can be used to provide meta data of a torrent
-     * @param magnetUri: magnet uri/link which can be used as an alternative to metainfoFile
-     * @param targetDirectory: the directory in which the torrent is stored
-     * @param seedAfterDownloaded: boolean, whether the client should seed the torrent after downloading has finished
-     * @param port: specific port for incoming connections, if empty default port is used
-     * @param downloadAllFiles: boolean, specifies whether all or only some files should be downloaded
+     * @param metainfoFile a .torrent-file which can be used to provide meta data of a torrent
+     * @param magnetUri magnet uri/link which can be used as an alternative to metainfoFile
+     * @param targetDirectory the directory in which the torrent is stored
+     * @param seedAfterDownloaded boolean, whether the client should seed the torrent after downloading has finished
+     * @param port specific port for incoming connections, if empty default port is used
+     * @param downloadAllFiles boolean, specifies whether all or only some files should be downloaded
      */
     @SuppressWarnings("all")
     public StreamOptions(String magnetUri, File metainfoFile, File targetDirectory, boolean seedAfterDownloaded, boolean downloadAllFiles, boolean useDefaultPort, Integer port, boolean useMagnetLink, boolean useTorrentFile) {
@@ -89,6 +93,16 @@ public class StreamOptions {
         }
     }
 
+    /**
+     * <p>The following constructor contains the most basic data needed to instantiate a version
+     * of the StreamOptions-class. It contains a magnet uri/link and a download location and no
+     * other data. The magnetUri could be replaced by a .torrent-file.</p>
+     *
+     * @see StreamOptions#StreamOptions(String, File, File, boolean, boolean, boolean, Integer, boolean, boolean)
+     * @since 0.00.2
+     * @param magnetUri magnet uri/link which can be used as an alternative to metainfoFile
+     * @param targetDirectory the directory in which the torrent is stored
+     */
     public StreamOptions(String magnetUri, File targetDirectory) {
         this.magnetUri = magnetUri;
         this.targetDirectory = targetDirectory;
@@ -100,9 +114,8 @@ public class StreamOptions {
      * no longer maintained.
      * </p>
      *
-     * @param magnetUri: magnet uri/link which can be used as an alternative to metainfoFile
+     * @param magnetUri magnet uri/link which can be used as an alternative to metainfoFile
      * @see StreamOptions#StreamOptions(String, File, File, boolean, boolean, boolean, Integer, boolean, boolean)
-     * {@link client.StreamOptions#StreamOptions(String, File, File, boolean, boolean, boolean, Integer, boolean, boolean) constructor with all possible parameters}
      * @since 0.00.4
      */
     @Deprecated
@@ -116,9 +129,8 @@ public class StreamOptions {
      * no longer maintained.
      * </p>
      *
-     * @param metainfoFile: a .torrent-file which can be used to provide meta data of a torrent
+     * @param metainfoFile a .torrent-file which can be used to provide meta data of a torrent
      * @see StreamOptions#StreamOptions(String, File, File, boolean, boolean, boolean, Integer, boolean, boolean)
-     * {@link client.StreamOptions#StreamOptions(String, File, File, boolean, boolean, boolean, Integer, boolean, boolean) constructor with all possible parameters}
      * @since 0.00.4
      */
     @Deprecated
@@ -127,9 +139,7 @@ public class StreamOptions {
     }
 
     /**
-     * Desc
-     *
-     * @return
+     * @return returns .torrent-file
      */
     File getMetainfoFile() {
         return this.metainfoFile;
@@ -178,7 +188,10 @@ public class StreamOptions {
     }
 
     /**
+     * Logging level for the return value of getLogLevel within this class
      *
+     * @see StreamOptions#getLogLevel()
+     * @since 0.00.1
      */
     public enum LogLevel {
         NORMAL,
