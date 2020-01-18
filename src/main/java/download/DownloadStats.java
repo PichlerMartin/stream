583e9b029149de311f13ebbd716e2e8c47f28b39
PichlerMartin <40146720150314@litec.ac.at>
2020-01-18 17:07:10 +0100

    finished javadoc for DownloadStats

--- src/main/java/download/DownloadStats.java
+++ src/main/java/download/DownloadStats.java
@@ -2,11 +2,26 @@ package download;
 
 import java.time.Duration;
 
+/**
+ * <p>this class serves as a provider for the download-stats, including the elapsed time,
+ * download rate and upload rate. for reasons of simplification the will be no extra documentation
+ * about the single field, instead they will be described upon the constructor header briefly</p>
+ *
+ * @author PichlerMartin
+ * @since december 2019
+ */
 public class DownloadStats {
     private final Duration elapsedTime;
     private final DownloadRate downloadRate;
     private final DownloadRate uploadRate;
 
+    /**
+     * <p>constructor method for download stats</p>
+     * @param elapsedTime the already elapsed time in seconds
+     * @param downloadRate the download rate served as DownloadRate object
+     * @param uploadRate the upload rate served as DownloadRate object
+     * @see DownloadRate
+     */
     public DownloadStats(Duration elapsedTime, DownloadRate downloadRate, DownloadRate uploadRate) {
         this.elapsedTime = elapsedTime;
         this.downloadRate = downloadRate;
