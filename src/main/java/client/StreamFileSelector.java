bd48156e367d761ef3967f5f2d6a74d3b1c20801
PichlerMartin <40146720150314@litec.ac.at>
2020-01-19 12:31:08 +0100

    finished javadoc on StreamFileSelector

--- src/main/java/client/StreamFileSelector.java
+++ src/main/java/client/StreamFileSelector.java
@@ -32,6 +32,13 @@ import static support.Globals.FORMAT_ILLEGAL_KEYPRESS;
  * the total torrent-package. it is used when the use selects to download not all files contained in
  * the torrent meta-info.</p>
  *
+ * <p>it contains two private field, atomic references, which can not be parallel accessed. first being called currentThread
+ * representing the current thread. thw second is called shutdown which is a boolean which indicates whether the download-process
+ * is already shut down.</p>
+ *
+ * @see TorrentFileSelector
+ * @see Initializable
+ *
  * @author Pichler Martin
  * @since december 2019
  */
@@ -40,11 +47,28 @@ public class StreamFileSelector extends TorrentFileSelector implements Initializ
     private AtomicReference<Thread> currentThread;
     private AtomicBoolean shutdown;
 
+    /**
+     * <p>initialisation of the private atomic reference fields</p>
+     *
+     * @see AtomicReference
+     *
+     * @author Pichler Martin
+     * @since december 2019
+     */
     StreamFileSelector() {
         this.currentThread = new AtomicReference<>(null);
         this.shutdown = new AtomicBoolean(false);
     }
 
+    /**
+     * <p>formats the correct output for the command line display of the torrent file selection. it shows a single
+     * line in which the user is asked  whether they want to download a specific file. this can be acquitted by either
+     * "y" for yes or "n" for no.</p>
+     *
+     * @see TorrentFile
+     * @param file a torrent file object
+     * @return the formatted string
+     */
     private static String getPromptMessage(TorrentFile file) {
         return format(FORMAT_DOWNLOAD_PART, join("/", file.getPathElements()));
     }
@@ -53,12 +77,16 @@ public class StreamFileSelector extends TorrentFileSelector implements Initializ
      * https://stackoverflow.com/questions/31416784/thread-with-lambda-expression
      * @param file: Torrent file containing information name, etc.
      * @return whether file is selected or nah
-     * FIXME:   try to implement display of unique parts on one site fxml window
+     *
+     * @deprecated makes a call to the private method {@link StreamFileSelector#showSinglePartStage(TorrentFile)} but
+     * this feature does not work properly yet. its purpose is to be called uniquely for every single part
+     * of the torrent-package, this function should be implemented in this method, until then it will stay deprecated
      */
     @Deprecated
-    private SelectionResult selectSinglePart(TorrentFile file) {
+    public SelectionResult selectSinglePart(TorrentFile file) {
         new Thread(() -> {
             try {
+                //  call to class private method which shows a new java fxml window for single part selection
                 this.showSinglePartStage(file);
             } catch (IOException e) {
                 // handle: log or throw in a wrapped RuntimeException
@@ -66,6 +94,7 @@ public class StreamFileSelector extends TorrentFileSelector implements Initializ
             }
         }).start();
 
+        //  placeholder, returns a random selection for torrent part, needs to be removed URGENTLY when single part selection is implemented
         boolean selectionResult = new Random().nextBoolean();
 
         if (selectionResult){
@@ -75,6 +104,26 @@ public class StreamFileSelector extends TorrentFileSelector implements Initializ
         }
     }
 
+    /**
+     * <p>this java fxml windows display a single part of a torrent file in a text-box with two buttons, "yes" and "no"
+     * which allow the user to select whether to download it or not.</p>
+     *
+     * <p>due to unknown reasons the fxml file is not correctly displayed after .show() is called, this error should be
+     * looked after if the ui selection of the torrent file should be realised, but until then this fxml window will not be
+     * used.</p>
+     * <hr><blockquote><pre>
+     *     secondStage.get().show();
+     * </pre></blockquote></hr>
+     *
+     * @param file a torrent file object
+     * @throws IOException exceptions is thrown if fxml file could not be found
+     * @see TorrentFile
+     * @see FXMLLoader
+     * @see UI_Controller_singlepart_page
+     *
+     * @author Pichler Martin
+     * @since january 2020
+     */
     private void showSinglePartStage(TorrentFile file) throws IOException {
         /*
          https://stackoverflow.com/questions/17850191/why-am-i-getting-java-lang-illegalstateexception-not-on-fx-application-thread
@@ -85,12 +134,15 @@ public class StreamFileSelector extends TorrentFileSelector implements Initializ
                 () -> secondStage.set(new Stage())
         );
 
+        //  loads the fxml file for this window
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../fxml/UI_stream_singlepart_page.fxml"));
         Parent root = loader.load();
 
+        //  sets the default stream background
         root.setStyle("-fx-background-image: url('/images/stream_LightModeUI_background.png'); -fx-background-repeat: no-repeat; -fx-background-size: 1215 765");
         UI_Controller_singlepart_page c = loader.getController();
 
+        //  initialises fxml controller with Torrent-file
         c.initData(file);
 
         c.setStage(secondStage.get());
@@ -102,9 +154,7 @@ public class StreamFileSelector extends TorrentFileSelector implements Initializ
         c.setParentStage(secondStage.get());
 
         /*
-
-        https://stackoverflow.com/questions/43647368/how-can-i-get-make-my-program-wait-until-javafx-window-has-been-closed-before-co
-
+            https://stackoverflow.com/questions/43647368/how-can-i-get-make-my-program-wait-until-javafx-window-has-been-closed-before-co
         */
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO);
@@ -119,7 +169,18 @@ public class StreamFileSelector extends TorrentFileSelector implements Initializ
         secondStage.get().show();
     }
 
-
+    /**
+     * <p>displays a query to ask the user if they want to download a specific file and accepts a single char as an answer.
+     * if they choose "y" it will be added in the switch-tree, if they answer "n" it will not be included in the build. every
+     * other input will trigger a error message in the command prompt and will result in an repetition of the query.</p>
+     * {@inheritDoc}
+     * @param file
+     * @return
+     * @see TorrentFileSelector#select(TorrentFile)
+     *
+     * @author Pichler Martin
+     * @since january 2020
+     */
     @Override
     protected SelectionResult select(TorrentFile file) {
         //  Method below is for the invocation of "UI_stream_singlepart_page.fxml"
@@ -127,10 +188,13 @@ public class StreamFileSelector extends TorrentFileSelector implements Initializ
         //  return this.selectSinglePart(file);
 
         while (!this.shutdown.get()) {
+            //  ask user whether to download a file
             System.out.println(getPromptMessage(file));
 
+            //  read input from user
             String nextCommand = this.readNextCommand(new Scanner(System.in));
 
+            //  check whether the users input was valid
             switch (this.readKey(nextCommand)) {
                 case 0:
                 case 1:
@@ -144,9 +208,21 @@ public class StreamFileSelector extends TorrentFileSelector implements Initializ
             }
         }
 
+        //  program should not reach this point, otherwise throw a exception
         throw new IllegalStateException("Shutdown");
     }
 
+    /**
+     * <p>this class private method evaluates the users input and check whether it is one of the allowed
+     * ones ("n", "N", "y", "Y") or if the input is invalid the calling method {@link StreamFileSelector#select(TorrentFile)}
+     * decides what to do depending on the input</p>
+     * @param nextCommand a string containing the input
+     * @return a byte (single digit number) which indicates the input result
+     *
+     * @see case
+     * @author Pichler Martin
+     * @since january 2020
+     */
     private byte readKey(String nextCommand) {
 
         byte result = -1;
@@ -180,19 +256,37 @@ public class StreamFileSelector extends TorrentFileSelector implements Initializ
         return result;
     }
 
+    /**
+     * <p>this class private method is used to read input from the command line</p>
+     * @param scanner a scanner object for reading the input from the command line
+     * @return a string containing the next command
+     * @see Scanner
+     *
+     * @author Pichler Martin
+     * @since january 2020
+     */
     private String readNextCommand(Scanner scanner) {
+        //  set the current thread
         this.currentThread.set(Thread.currentThread());
 
         String nextCommand;
         try {
+            //  read next line
             nextCommand = scanner.nextLine().trim();
         } finally {
+            //  terminate this thread
             this.currentThread.set(null);
         }
 
         return nextCommand;
     }
 
+    /**
+     * <p>shut down the torrent file selection, in case an invalid operation has been performed</p>
+     *
+     * @author Pichler Martin
+     * @since january 2020
+     */
     void shutdown() {
         this.shutdown.set(true);
         Thread currentThread = this.currentThread.get();
@@ -202,6 +296,14 @@ public class StreamFileSelector extends TorrentFileSelector implements Initializ
 
     }
 
+    /**
+     * {@inheritDoc}
+     * @param location
+     * @param resources
+     *
+     * @author Pichler Martin
+     * @since january 2020
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
