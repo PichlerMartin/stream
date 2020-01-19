762b2f14f5ee6be8e95fc8652d143cd2d62e2d71
PichlerMartin <40146720150314@litec.ac.at>
2020-01-19 12:51:40 +0100

    finished javadoc on LibraryDescription.java

--- src/main/java/filelibrary/LibraryDescription.java
+++ src/main/java/filelibrary/LibraryDescription.java
@@ -1,4 +1,15 @@
 package filelibrary;
 
+import javax.annotation.Untainted;
+
+/**
+ * <p>a class concept which should provide more accurate description of a object of {@link Library},
+ * some fields and methods still need to be implemented, it is not needed urgently</p>
+ *
+ * @author Pichler Martin
+ * @since november 2019
+ */
+@Untainted
+@SuppressWarnings("unused")
 public class LibraryDescription {
 }
